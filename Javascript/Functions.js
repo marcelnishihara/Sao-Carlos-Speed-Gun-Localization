61271dfac355d6476c4ae9c1b83e9cbe07ae8332
Marcel Nishihara <marcelnishihara@gmail.com>
2019-05-14 00:08:04 -0300

    Javascript Version of Program Added | Python Version to Develop

---
+++ Javascript/Functions.js
@@ -0,0 +1,60 @@
+function saoCarlosAgora(URL) {
+  var localizacaoRadarSCA = new Array();
+
+  var fetchUrl = UrlFetchApp.fetch(URL);
+  var contentText = fetchUrl.getContentText();
+
+  var regexData = /Radar \|(.*)<\/h2>/gm;
+  var dataLocalizacao = regexData.exec(contentText);
+  localizacaoRadarSCA.push(dataLocalizacao[1].replace(/(\s)/,''));
+
+  var regexSectionRadar = /<section class="radar">(.*)<\/section>/;
+  var sectionRadar = regexSectionRadar.exec(contentText);
+
+  var regexRadares = /<\/big><div><h4>(.*?)<\/h4>/gm;
+
+  while (regexRadares.exec(sectionRadar) != null) {
+    var radares = regexRadares.exec(sectionRadar);
+    localizacaoRadarSCA.push(radares[1].replace(/(\s)/,''));
+  }
+
+  return localizacaoRadarSCA;
+
+}
+
+
+
+function saoCarlosOficial(URL) {
+  var localizacaoRadarSCO = new Array();
+  
+  var fetchUrl = UrlFetchApp.fetch(URL);
+  var contentText = fetchUrl.getContentText();
+  
+  var regexData = /<font face="Verdana" size="3"><b>(..\/..) -.*<\/b><br><\/font>/;
+  var regexRadarUm = /Radar 1 - (.*)<br>/;
+  var regexRadarDois = /Radar 2 - (.*)<br>/;
+  var regexRadarTres = /Radar 3 - (.*)<br>/;
+  
+  localizacaoRadarSCO[0] = regexData.exec(contentText)[1];
+  localizacaoRadarSCO[1] = regexRadarUm.exec(contentText)[1];
+  localizacaoRadarSCO[2] = regexRadarDois.exec(contentText)[1];
+  localizacaoRadarSCO[3] = regexRadarTres.exec(contentText)[1];
+  
+  return localizacaoRadarSCO;
+}
+
+
+
+function writeToSheet(infoToWrite, sheet) {
+  var number = 1;
+  
+  var openSpreadsheet = SpreadsheetApp.openById(ID_SPREADSHEET);
+  var sheetByName = openSpreadsheet.getSheetByName(sheet);
+  var header = ['Data', 'Radar 1', 'Radar 2', 'Radar 3'];
+  var lastRow = sheetByName.getLastRow() + number;
+
+  sheetByName.getRange(1, 1, 1, header.length).setValues([header]);  
+  sheetByName.getRange(lastRow, 1, 1, 4).setValues([infoToWrite]);
+  sheetByName.autoResizeColumns(1, infoToWrite.length);
+
+}
