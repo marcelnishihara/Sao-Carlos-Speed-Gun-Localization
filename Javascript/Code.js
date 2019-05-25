e30083786ec3b07b22fe3e441f76c9597235e510
Marcel Nishihara <marcelnishihara@gmail.com>
2019-05-25 13:22:42 -0300

    Files renamed

--- Javascript/Code.js
+++
@@ -1,9 +0,0 @@
-function localizacaoRadar() {
-     
-  var infoSCA = saoCarlosAgora(URL_SCA);
-  var infoSCO = saoCarlosOficial(URL_SCO);
-  
-  writeToSheet(infoSCA, SHEET_NAME_SCA);
-  writeToSheet(infoSCO, SHEET_NAME_SCO);
-
-}
