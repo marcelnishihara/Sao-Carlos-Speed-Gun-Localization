da096f100b3ba272b677ef51e42b4052e67c8627
Marcel Nishihara <marcelnishihara@gmail.com>
2020-03-27 02:52:16 -0300

    Python Version 1.0 | JS is Broken

--- Python/functions.py
+++ Python/functions.py
@@ -1,36 +1,75 @@
+import requests, re
 from globals import *
-import urllib3
-import re
 
-def main():
-    html = return_html(source)
-    get_localization(html)
 
-def return_html(site):
-    http = urllib3.PoolManager()
-    data_request = http.request('GET', site)
-    site_status = data_request.status
+def main(request):
+    try:
+        html = return_html(source)
+        local_and_speed_limit = get_localization_and_speed(html)
+        has_data = bool(local_and_speed_limit)
 
-    if site_status == 200:
-        html_file = data_request.data.decode("utf-8")
+        if has_data:
+            data_captured_msg = f"The program reach the data\nConsider drive carefully anyway."
+            pretty_print(data_captured_msg, True)
+            print(local_and_speed_limit)
+            return local_and_speed_limit
+
+    except:
+        exception_msg = f"Something Went Wrong.\nDrive Slowly."
+        pretty_print(exception_msg, False)
+        return exception_msg
+
+
+def return_html(source):
+    accessing_msg = f"Trying to Access {source}"
+    pretty_print(accessing_msg, True)
+
+    data_request = requests.get(source)
+    source_status = data_request.status_code
+
+    if source_status == 200:
+        success_message = f"Status Code: {source_status}\n{source} was accessed successfully.\nGetting Speed Gun Data."
+        pretty_print(success_message, True)
+        html_file = data_request.text
         return html_file
     else:
-        failure_message = "The status code of the site {} is: {}".format(site, site_status)
-        print(failure_message)
+        failure_message = f"Error.\nThe status code of the site {source} is: {source_status}\nExiting program."
+        pretty_print(failure_message, False)
         exit()
 
-def get_localization(h):
-    regex_date = re.compile(r"Radar \| (.*)<\/h2>")
-    regex_speed_limits = re.compile(r"<big>(.{2}) <small>")
-    regex_section_radar = re.compile(r"<\/big><div><h4>(.*?)<\/h4>")
 
-    date_match = regex_date.search(h)
-    speed_limits_matches = regex_speed_limits.finditer(h)
-    section_radar_matches = regex_section_radar.finditer(h)
+def get_localization_and_speed(html):
+    regex_localization_and_speed = re.findall(regex, html)
+    localization_and_speed = {}
+    radar_number = 0
+    
+    for tuple_value in regex_localization_and_speed:
+        for index, value in enumerate(tuple_value):
+            is_even = (index % 2) == 0
 
-    print("Data: {}".format(date_match.group(1)))
+            if is_even:
+                current_radar = {}
+                
+                speed_limit = int(tuple_value[index].strip())
+                road = tuple_value[index + 1].strip().lower()
 
-    for i in section_radar_matches:
-        for j in speed_limits_matches:
-            print("Localização: {}, Limite de Velocidade: {} km/h".format(i.group(1), j.group(1)))
-            
+                msg = f"Road: {road} | Speed Limit:{speed_limit}"
+
+                current_radar = {
+                    "localization" : road,
+                    "speed_limit" : speed_limit
+                }
+
+                radar_number += 1
+                localization_and_speed[radar_number] = current_radar
+
+    return localization_and_speed
+
+
+def pretty_print(msg, status):
+    if status == True:
+        format_msg = f"{colors['success']}{msg}{colors['end']}"
+    else :
+        format_msg = f"{colors['err']}{msg}{colors['end']}"
+
+    print(format_msg, end="\n\n")
--- Python/globals.py
+++ Python/globals.py
@@ -1,1 +1,9 @@
-source = "https://saocarlosagora.com.br"
+source = "https://saocarlosagora.com.br/"
+regex = r".*radar.>.*<ul>.*<li><big>([0-9]{2}).*<h4>(.*)<\/h4>.*<li><big>([0-9]{2}).*<h4>(.*)<\/h4>.*<li><big>([0-9]{2}).*<h4>(.*)<\/h4>"
+
+colors = {
+    "err" : "\033[91m",
+    "end" : "\033[m",
+    "success" : "\033[92m",
+    "warning" : "\033[93m"
+}
